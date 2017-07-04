6b721f5f04696eb211c1aca0799afd0cebd80714
luisantonioig <c-remix@hotmail.es>
2017-07-03 23:06:56 -0500

    Ingresar la contraseña para obtener la hmac

--- buildHmac.py
+++ buildHmac.py
@@ -2,7 +2,7 @@ import hmac
 import base64
 
 url = "https://sandbox-authservice.priaid.ch/login"
-password = ""
+password = input('Escribe tu contraseña:')
 rawHashString = hmac.new(bytes(password, encoding='utf-8'),
                          url.encode('utf-8')).digest()
 computedHashString = base64.b64encode(rawHashString).decode()
